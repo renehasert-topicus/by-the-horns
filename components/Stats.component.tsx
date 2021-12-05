33ac7f29d97d59ac732c59dc69d7a99e3a5d89bc
renehasert-topicus <rene.hasert@topicus.nl>
2021-12-05 20:30:38 +0100

    stats

--- components/Stats.component.tsx
+++ components/Stats.component.tsx
@@ -1,24 +1,24 @@
 import React, { ReactElement } from 'react';
-import testimonialsStyles from '../styles/Testimonials.module.scss';
+import statsStyles from '../styles/Stats.module.scss';
 import TooltipComponent from './Tooltip.component';
 import { useMediaQuery } from 'react-responsive'
 
-function TestimonialsComponent(): ReactElement {
+function StatsComponent(): ReactElement {
     const desktopScreen = useMediaQuery({ query: '(min-width: 768px)' })
     return (
-        <div className={testimonialsStyles.testimonials}>
+        <div className={statsStyles.stats}>
             <TooltipComponent message="This percentage is probably accurate." direction={desktopScreen ? 'left' : 'bottom'}>
-                <div className={testimonialsStyles.testimonials__testimonial}>
-                <span className={testimonialsStyles.testimonials__testimonial__number}>
+                <div className={statsStyles.stats__stat}>
+                <span className={statsStyles.stats__stat__number}>
                     97%
                 </span>
                     satisfaction rate
                 </div>
             </TooltipComponent>
             <TooltipComponent message="Terms and conditions apply." direction={desktopScreen ? 'left' : 'bottom'}>
-                <div className={testimonialsStyles.testimonials__testimonial}>
+                <div className={statsStyles.stats__stat}>
                     at least
-                    <span className={testimonialsStyles.testimonials__testimonial__number}>
+                    <span className={statsStyles.stats__stat__number}>
                     1
                 </span>
                     hire per visit
@@ -28,4 +28,4 @@ function TestimonialsComponent(): ReactElement {
     );
 }
 
-export default TestimonialsComponent;
+export default StatsComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
-import homeStyles from '../styles/Home.module.scss';
 import React, { ReactElement } from 'react';
+import { GetStaticProps } from 'next';
 import HeroComponent from '../components/Hero.component';
 import ActionWallComponent from '../components/ActionWall.component';
-import TestimonialsComponent from '../components/Testimonials.component';
+import StatsComponent from '../components/Stats.component';
 import NavComponent from '../components/Nav.component';
-import { applicantsQuery } from '../queries/applicants.query';
-import { GetStaticProps } from 'next';
-import { sanityClient } from '../lib/sanity.server';
 import CarouselComponent from '../components/Carousel.component';
+import { applicantsQuery } from '../queries/applicants.query';
+import { sanityClient } from '../lib/sanity.server';
 import { Applicant } from '../models/Applicant.model';
+import homeStyles from '../styles/Home.module.scss';
 
 function Home({ applicants }: { applicants: Applicant[] }): ReactElement {
     return (
         <div id="home" className={homeStyles.container}>
             <NavComponent/>
             <HeroComponent>
-                <TestimonialsComponent/>
+                <StatsComponent/>
                 <ActionWallComponent/>
             </HeroComponent>
             <section id="carousel" style={{ height: '100rem', zIndex: -1 }}>
